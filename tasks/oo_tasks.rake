1db3076c5ad5c5d01a542e5002f886f29dabe3f3
unnu <norman.timmler@gmail.com>
2009-10-28 14:26:31 +0100

    cleaned up initial commit

--- tasks/oo_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :oo do
-#   # Task goes here
-# end
--- test/ruby_fixtures_test.rb
+++ test/ruby_fixtures_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class OoTest < ActiveSupport::TestCase
+class RubyFixturesTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
